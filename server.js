f84e2bcb9fc61959c237f11fb242c1e2e73aeec4
mayaG84 <mayb84@gmail.com>
2026-02-10 15:26:47 +0100

    fix(deploy): route in server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/EcoRide-Front')));
+app.use(express.static(path.join(__dirname, 'dist/EcoRide-Front/browser')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/EcoRide-Front/index.html'));
+app.get(/^\/.*$/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/EcoRide-Front/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server runs ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
